0d69e3cfc63d9c78c5d527280a3754fe3931eeb8
Glen Small <14293295+glensmall@users.noreply.github.com>
2021-01-09 17:52:03 +0000

    adding table formatting and cleaning up function calls

--- fileHashChecker.go
+++ fileHashChecker.go
@@ -6,146 +6,87 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/hex"
-	"fmt"
+	"hash"
 	"io"
 	"os"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 // Main Entrypoint for this application
 func main() {
 
 	inputFile := os.Args[1]
-	inputHash := os.Args[2]
 
-	computedHash := computeSHA256(inputFile)
+	computeSingleHash(inputFile, "SHA256")
+	computeSingleHash(inputFile, "SHA512")
+	computeSingleHash(inputFile, "SHA1")
+	computeSingleHash(inputFile, "MD5")
 
-	if inputHash == computedHash {
-		fmt.Println("Hashes Match")
-	} else {
-		fmt.Println(fmt.Sprintf("Hashes do not match.  \n\tInput = %s\n\tComputed = %s", inputHash, computedHash))
+	return
+}
+
+// function to compute a single hash and return it
+func computeSingleHash(inputFile string, hashType string) {
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Filename", "Hash", "Hash Type"})
+
+	if hashType == "SHA256" {
+		h := sha256.New()
+		computedHash := computeHash(inputFile, h)
+
+		t.AppendRows([]table.Row{
+			{inputFile, computedHash, "SHA256"},
+		})
+
+	} else if hashType == "SHA512" {
+		h := sha512.New()
+		computedHash := computeHash(inputFile, h)
+
+		t.AppendRows([]table.Row{
+			{inputFile, computedHash, "SHA512"},
+		})
+	} else if hashType == "SHA1" {
+		h := sha1.New()
+		computedHash := computeHash(inputFile, h)
+
+		t.AppendRows([]table.Row{
+			{inputFile, computedHash, "SHA1"},
+		})
+	} else if hashType == "MD5" {
+		h := md5.New()
+		computedHash := computeHash(inputFile, h)
+
+		t.AppendRows([]table.Row{
+			{inputFile, computedHash, "MD5"},
+		})
 	}
+
+	t.AppendSeparator()
+	t.Render()
 
 	return
 }
 
 // function to compute a hash based on the specified file
-// takes a filename
+// takes a filename and a hash.Hash from the specific crypto class
 // returns the computed checksum and and error object
-func computeSHA256(filename string) string {
+func computeHash(filename string, h hash.Hash) string {
 
 	f, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
 
+	// defer closing the file until we exit this method
 	defer func() {
 		_ = f.Close()
 	}()
 
 	buf := make([]byte, 1024*1024)
-	h := sha256.New()
-
-	for {
-		bytesRead, err := f.Read(buf)
-		if err != nil {
-			if err != io.EOF {
-				panic(err)
-			}
-
-			break
-		}
-
-		h.Write(buf[:bytesRead])
-	}
-
-	// return the computed string
-	return (hex.EncodeToString(h.Sum(nil)))
-}
-
-// function to compute a hash based on the specified file
-// takes a filename
-// returns the computed checksum and and error object
-func computeSHA512(filename string) string {
-
-	f, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-
-	defer func() {
-		_ = f.Close()
-	}()
-
-	buf := make([]byte, 1024*1024)
-	h := sha512.New()
-
-	for {
-		bytesRead, err := f.Read(buf)
-		if err != nil {
-			if err != io.EOF {
-				panic(err)
-			}
-
-			break
-		}
-
-		h.Write(buf[:bytesRead])
-	}
-
-	// return the computed string
-	return (hex.EncodeToString(h.Sum(nil)))
-}
-
-// function to compute a hash based on the specified file
-// takes a filename
-// returns the computed checksum and and error object
-func computeSHA1(filename string) string {
-
-	f, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-
-	defer func() {
-		_ = f.Close()
-	}()
-
-	buf := make([]byte, 1024*1024)
-	h := sha1.New()
-
-	for {
-		bytesRead, err := f.Read(buf)
-		if err != nil {
-			if err != io.EOF {
-				panic(err)
-			}
-
-			break
-		}
-
-		h.Write(buf[:bytesRead])
-	}
-
-	// return the computed string
-	return (hex.EncodeToString(h.Sum(nil)))
-}
-
-// function to compute a hash based on the specified file
-// takes a filename
-// returns the computed checksum and and error object
-func computeMD5(filename string) string {
-
-	f, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-
-	defer func() {
-		_ = f.Close()
-	}()
-
-	buf := make([]byte, 1024*1024)
-	h := md5.New()
+	// here
 
 	for {
 		bytesRead, err := f.Read(buf)
